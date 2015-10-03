a1bc877918ef570fdd839a7ab8a580ab840a9445
Jonathan D. Lace <jdlace@icloud.com>
2015-10-03 16:21:00 -0400

    initial commit

--- Open House 2015/main.swift
+++ Open House 2015/main.swift
@@ -8,5 +8,38 @@
 
 import Foundation
 
-print("Hello, World!")
+var inputStr: String
+var inputInt: Int
+
+print("Welcome to Seton Hall Prep Open House 2015!\nPlease enter your name to continue.")
+
+inputStr = inputString()
+
+print("Welcome, " + inputStr)
+
+print("Please enter your age:")
+
+inputInt = inputInteger()
+
+
+if inputInt > 18 && inputInt <= 25 {
+    
+    print("I see you're probably out of college. 🍻\n")
+
+} else if inputInt <= 62 && inputInt >= 25 {
+    
+    print("I see you're hard at work. 😁\n")
+    
+} else if inputInt <= 75 && inputInt >= 62 {
+    
+    print("I see you're probably enjoying retirement. 😎\n")
+    
+}
+
+print("Which department would you like to know more about?")
+print("1-Theology\n2-English\n3-History")
+
+if inputInteger() == 1 {
+    print("Theology is the queen of the sciences.")
+}
 
