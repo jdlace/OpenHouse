347f37157b4bb9aa934b515842f9d3b0caa9219c
Jonathan D. Lace <jdlace@icloud.com>
2015-10-03 15:38:50 -0400

    Initial Commit

---
+++ Open House 2015/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Open House 2015
+//
+//  Created by Jonathan Lace on 10/3/15.
+//  Copyright © 2015 shp. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
